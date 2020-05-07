44eae861e40ac1c4daadd83254f5fa018fc90c2e
Reinaldo Ramos <masterrey@gmail.com>
2020-05-06 22:14:33 -0300

    Ajuste e pulo

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float DragWoY=5;
+    public float WalkForce = 10;
+    public float JumpForce = 10;
     public enum States
     {
         Idle,
         Walk,
         Attack,
         Damage,
-        Die
+        Die,
+        Jump
+
     }
     public States state;
     public Animator anim;
     public Rigidbody2D rdb;
     float horizontal;
+    float jumpTime=1;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,8 +41,25 @@ public class PlayerControl : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
         horizontal = Mathf.Abs(horizontal);
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(Jump());
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+           jumpTime = 0;
+        }
+        anim.SetFloat("DistChao", Physics2D.Raycast(transform.position, Vector2.down, 10).distance);
+
     }
 
+    void FixedUpdate()
+    {
+        //drag sem o y 
+        rdb.AddForce(new Vector2(-rdb.velocity.x,0)* DragWoY);
+    }
+
+        
     IEnumerator Idle()
     {
         state = States.Idle;
@@ -47,8 +70,9 @@ public class PlayerControl : MonoBehaviour
             if (horizontal > 0.1f)
             {
                 StartCoroutine(Walk());
+
             }
-           
+            
         }
            
     }
@@ -62,13 +86,40 @@ public class PlayerControl : MonoBehaviour
         while (state == States.Walk)
         {
             yield return new WaitForEndOfFrame();
-            rdb.AddForce(transform.right*10);
+            rdb.AddForce(transform.right* WalkForce);
 
             if (horizontal < 0.1f)
             {
                 StartCoroutine(Idle());
             }
+          
+
+        }
+
+    }
+
+    IEnumerator Jump()
+    {
+        
+        state = States.Jump;
+        jumpTime = .3f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 10);
+        if (hit.distance > 1.3f)
+        {
+            StartCoroutine(Idle());
+        }
+        while (state == States.Jump)
+        {
+            yield return new WaitForFixedUpdate();
+            rdb.AddForce(transform.up * JumpForce * jumpTime);
             
+            jumpTime -= Time.fixedDeltaTime;
+
+            if (jumpTime < 0.1f)
+            {
+                StartCoroutine(Idle());
+            }
+
         }
 
     }
